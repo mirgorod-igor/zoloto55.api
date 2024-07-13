34ba303b97e3fd345dbc94dfb1c265c2f9349536
mirgorod.igor@mail.ru <mirgorod.igor@mail.ru>
2024-07-13 08:38:15 +0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
     console.log('PORT=' + process.env.PORT)
 
-    await app.listen(process.env.PORT || 3000)
+    await app.listen(/*process.env.PORT || 3000*/443)
 }
 
 bootstrap()
